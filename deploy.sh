8733f2f090299e2cc9b3eeff9d6b59afbe31b985
Anjolaoluwa Olubusi <anjolaolubusi@gmail.com>
2022-07-03 17:05:07 -0500

    Added Deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+
+npm run build
+git checkout --orphan gh-pages
+git --work-tree dist add --all
+git --work-tree dist commit -m "gh-pages"
+git push origin HEAD:gh-pages --force
+rm -r dist
+git checkout -f master
+git branch -D gh-pages
